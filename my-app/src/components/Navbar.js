a32a95a5acd403d6352c42e119f31ae1ade8d696
Joaquin Paolo <joaquinpaolo1@gmail.com>
2022-06-21 21:50:46 -0300

    first commit

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+function Navbar() {
+    return(
+        <nav class="navbar">
+            <h1>Kira Burger</h1>
+        <div class="navbar-nav">
+            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="#">Burgers</a>
+            <a class="nav-link" href="#">Beers</a>
+            <a class="nav-link" href="#">About As</a>
+          </div>
+      </nav>
+    )}
+
+export default Navbar;
